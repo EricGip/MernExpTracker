715f3c5f717cea8ef7411afab8a49dc12e147be5
eric <ericgip97@gmail.com>
2020-06-30 12:14:15 -0700

    github to heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const morgan = require("morgan");
 const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
-
+ 
 connectDB();
 
 const transactions = require("./routes/transactions");
